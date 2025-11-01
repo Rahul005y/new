4e4de7a72c2c68ca4d7e49a94a991b2e6e2d5429
Rahul yadav <Rahulyadavskn9555@gmail.com>
2025-11-01 09:35:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let h1 = document.querySelector("h1");
 
+window.addEventListener("keydown", function(kuch) {
+  if (kuch.key === " ") {
+    h1.textContent = "SPC"; // shows "SPC" when space is pressed
+  } else {
+    h1.textContent = kuch.key; // shows other key names
+  }
+
+  console.log(kuch.key);
+});
